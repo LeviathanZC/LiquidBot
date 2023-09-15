0d450b582218927c9ddebc809f88e5c601dbe000
LeviathanZC <leviathan.zercomp@gmail.com>
2023-09-15 21:17:20 +0300

    Initial commit

---
+++ src/main/java/com/ecorp/LiquidBot/LiquidBotApplication.java
@@ -0,0 +1,13 @@
+package com.ecorp.LiquidBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecorp/LiquidBot/LiquidBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecorp.LiquidBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
